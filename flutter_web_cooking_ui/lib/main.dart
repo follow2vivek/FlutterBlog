385eac82adf93beb1a55cbc1cb23bd9cb4b6d251
Vivek <vshrm1994@gmail.com>
2019-05-15 12:07:10 +0530

    project uploaded

--- flutter_web_cooking_ui/lib/main.dart
+++ flutter_web_cooking_ui/lib/main.dart
@@ -1,25 +1,128 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_ui/widgets/add_money_screen.dart';
+import 'package:flutter_web/material.dart';
+import 'package:flutter_web/rendering.dart';
+import 'package:ui_design/image_url.dart';
 
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Demo',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
 }
 
-class Home extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'CookingShooking',
+          style: TextStyle(
+            color: Colors.black54,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: <Widget>[
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'Home',
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'Service',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'About Us',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'Contact Us',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
       body: Container(
-        child: Center(
-          child: FlatButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (_){
-              return AddMoneyScreen();
-            }));
-          }, child: Text('Go')),
+        padding: EdgeInsets.all(16),
+        width: MediaQuery.of(context).size.width,
+        color: Colors.grey[100],
+        child: GridView.builder(
+          itemCount: imageUrl.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              childAspectRatio: MediaQuery.of(context).size.width *
+                  0.8 /
+                  MediaQuery.of(context).size.height *
+                  0.8),
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 500,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 2,
+                    spreadRadius: 2,
+                    color: Colors.black.withOpacity(0.1),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                    imageUrl[index],
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
--- flutter_web_cooking_ui/lib/navbar/titled_navigation_bar.dart
+++
@@ -1,4 +0,0 @@
-library titled_navigation_bar;
-
-export './src/navigation_bar.dart';
-export './src/navigation_bar_item.dart';
